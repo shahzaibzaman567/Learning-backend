d52847e0d975a0a90ef762c03920f5440cbc3417
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-11-11 06:29:44 -0800

    first chapter of introduction and basic work are completed

---
+++ chapter-1/index.js
@@ -0,0 +1,72 @@
+const lib = require("./lib.js");
+
+//for import the express 
+const express = require("express");
+console.log("hello world from shahzaib");//simple console for checking 
+//server banana ka liya koi bi number dal sakta ha 
+const server = express();
+server.listen(465);//server number 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("hello")
+// let a = 5;
+
+//use for simple js 
+// console.log(lib.Add(2,3),libSub(4,5))
+//use for ES Ecmascript
+// import { Sub,Add } from "./lib.js";
+
+// const fs = require("fs");
+// const  t1=performance.now()
+
+// const txt = fs.readFileSync("demo.txt","utf-8");
+// console.log(txt)
+
+// fs.readFile("demo.txt","utf-8",(err,txt)=>{
+// console.log(txt)
+// })
+
+// const Number = 456 +555;
+// const  t2=performance.now()
+
+// console.log(Number);
+// console.log(t1-t2);
+
+
+
+
---
+++ chapter-1/lib.js
@@ -0,0 +1,28 @@
+//export ak kisam ka obj hota ha ya exports ka matlab moduls or . is ma sum jo ha 
+// os ko export karo .
+
+// use in Es
+
+
+//use in js 
+
+exports.Add = (a, b) => {
+    return a + b;
+}
+
+
+exports.Sub = (a, b) => {
+    return a - b;
+}    
+// const Add = (a, b) => {
+//     return a + b;
+// }
+
+
+// const Sub = (a, b) => {
+//     return a - b;
+// }
+
+
+// export {Sub,Add};
+
