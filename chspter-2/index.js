4f127a772ee652497124f0208dcd15bc6c687e44
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-11-17 19:17:42 -0800

    Dynamically print the product in chapter 2 folder

--- chspter-2/index.js
+++ chspter-2/index.js
@@ -3,12 +3,34 @@ const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const product = data.products[0];
+const products = data.products;
 //i creat a response to a server req and res are importan and its place
 // provide are very important
 const User = { age: 5 };
 //HTTP :Hyper Text Transfer protocol
 const server = http.createServer((req, res) => {
+  // console.log(product.thumbnail)
+  if (req.url.startsWith("/product")) {
+    const urlPath = req.url.split("/");
+    const Id = Number(urlPath[2]);
+    const prd = products.find((p) => p.id === (Id));
+    console.log("I am prd",prd)
+
+    //if product not found throught 404
+    if (!prd) {
+    res.writeHead(404, "Product Not Found");
+    return res.end("Error 404 product not");
+  }
+    res.setHeader("Content-Type", "text/html");
+
+    let modifiedIndex = 
+    index.replace("**title**",prd.title)
+    .replace("**price**",prd.price)
+    .replace("**url**",prd.thumbnail)
+    res.end(modifiedIndex);
+    return;
+  }
+
   switch (req.url) {
     case "/":
       res.setHeader("Content-Type", "text/html");
@@ -19,14 +41,10 @@ const server = http.createServer((req, res) => {
       // res.setHeader("Dummy", "DummyValue");
       res.end(JSON.stringify(data));
       break;
-          case "/product":
-      res.setHeader("Content-Type", "text/html");
-      let modifiedIndex = index.replace("**title**",product.title).replace("**price**",product.price)
-      res.end(modifiedIndex);
-      break;
-      default:
-        res.writeHead(404,"NT Found")
-        res.end("404 Error")
+
+    default:
+      res.writeHead(404, "NT Found");
+      res.end("404 Error");
   }
 
   //     res.end("<h1>hello</h1>");
