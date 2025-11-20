acb4772e4245497edc1d9be325ec62f7cd0fe290
shahzaibzaman567 <shahzaibzaman567@gmail.com>
2025-11-20 19:30:43 +0500

    install morgan and practice with put get request  etc in chapter 3

--- chapter-3/index.js
+++ chapter-3/index.js
@@ -3,3 +3,79 @@ const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = data.products;
+const morgan = require("morgan");
+
+//Creat a server with expressjs
+const express = require("express");
+const server = express();
+
+
+
+//-------------------------------- API - Endpoint - Route -----------------------------------//
+
+// middleWere is a squance 
+//jab ak kam ham check kara jasa country of user to wo match na kara to middleWare is return karta ha or agar 
+//match ho jaya to dosra middle ware ko pohoncha data ha 
+//Execut any code
+//Make change to the reques and the response object 
+
+//bodyParer
+server.use(express.json());
+// server.use(express.urlencoded());               // ---------------- use with form submiting
+
+server.use(express.static("public"));
+
+
+server.use((req,res,next)=>{
+  console.log(req.method,req.ip,req.hostname,new Date ,req.get("User-Agent"))
+  next()
+})
+
+const auth =(req,res,next)=>{
+
+  console.log(req.query)
+  if(req.query.Password=='123'){
+
+  next()
+}else{
+res.sendStatus(401)
+}
+}
+
+
+//Data  Get 
+server.get("/",auth ,(req, res) => {
+  res.json({type:"GET"})
+})
+//Data  Put 
+server.put("/", (req, res) => {
+  res.json({type:"PUT"})
+})
+//Data  POST 
+
+server.post("/",auth, (req, res) => {
+  res.json({type:"POST"})
+})
+//Data  PATCH 
+
+server.patch("/", (req, res) => {
+  res.json({type:"PATCH"})
+})
+//Data  DELET 
+
+server.delete("/", (req, res) => {
+  res.json({type:"DELET"})
+})
+
+//Demon
+server.get("/", (req, res) => {
+  // res.send("<h1>Hello<h1>")
+  res.status(201).send("<h1>Hello<h1>")
+  // res.sendFile("/(SMIT)-course/Learning-backend/chapter-3/index.html")
+  // res.json(products);
+  // res.sendStatus(404)
+});
+
+server.listen(5173, () => {
+  console.log("Srever Started");
+});
